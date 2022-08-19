063bfca83ae010cc377e749561d9eaf2d8e46834
Kostya Pechenenko <kpechenenko@gmail.com>
2022-08-20 05:34:41 +0700

    feat: create program entry point

---
+++ src/main/java/ru/kpechenenko/register/bot/Program.java
@@ -0,0 +1,18 @@
+package ru.kpechenenko.register.bot;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import ru.kpechenenko.register.bot.config.BotConfig;
+
+public final class Program {
+    public static void main(String[] args) {
+        try {
+            var applicationContext = new AnnotationConfigApplicationContext(BotConfig.class);
+            var currencyBot = applicationContext.getBean(PrintServiceRegisterBot.class);
+            var telegramBotsApi = applicationContext.getBean(TelegramBotsApi.class);
+            telegramBotsApi.registerBot(currencyBot);
+        } catch (Exception e) {
+            System.err.printf("ERROR! %s\n", e.getMessage());
+        }
+    }
+}
